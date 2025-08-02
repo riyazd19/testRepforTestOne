e8b33731335653daf6d09c5e37e197b64c4acfb5
riyazd19 <91775182+riyazd19@users.noreply.github.com>
2025-08-02 12:18:21 +0530

    FirstCommentInRiyazBranch

---
+++ CreatingPackageInRiyazBranch/FirstProgInRiyazBranch.java
@@ -0,0 +1,8 @@
+package CreatingPackageInRiyazBranch;
+
+public class FirstProgInRiyazBranch {
+    public static void main(String[] args) {
+        System.out.println("FirstProgInRiyazBranch");
+    }
+    
+}
